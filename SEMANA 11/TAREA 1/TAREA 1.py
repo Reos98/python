09a3150a031340c1a433dc09c29d623083af5bdb
Byron Montoya <byrondark36@gmail.com>
2024-09-01 11:56:55 -0500

    TAREAS DE LA SEMANA 11

---
+++ SEMANA 11/TAREA 1/TAREA 1.py
@@ -0,0 +1,29 @@
+# Crear una matriz bidimensional (3x3) para el ejemplo.txt
+matriz = [
+    [6, 3, 4],
+    [1, 8, 5],
+    [2, 9, 7]
+]
+
+# Valor que estamos buscando
+valor_buscado = 6
+
+# Inicializar variables para rastrear la posición del valor
+fila_encontrada = -1
+columna_encontrada = -1
+
+# Recorrer la matriz para buscar el valor
+for fila in range(len(matriz)):
+    for columna in range(len(matriz[fila])):
+        if matriz[fila][columna] == valor_buscado:
+            fila_encontrada = fila
+            columna_encontrada = columna
+            break  # Detener la búsqueda una vez que se encuentra el valor
+    if fila_encontrada != -1:
+        break  # Salir del bucle exterior si se encuentra el valor
+
+# Verificar si se encontró el valor y mostrar la posición
+if fila_encontrada != -1:
+    print(f"Se encontró {valor_buscado} en la fila {fila_encontrada} y columna {columna_encontrada}.")
+else:
+    print(f"{valor_buscado} no se encontró en la matriz.")
